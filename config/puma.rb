cf2498f69d567016e486f00865cd41fbf82f133e
watsuyo <dmwtpdmwtp24@gmail.com>
2021-05-09 14:20:52 +0000

    Use SSL and the Puma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,14 @@
 # Puma can serve each request in a thread from an internal thread pool.
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "production" }
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+preload_app!
+plugin :tmp_restart
+
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
